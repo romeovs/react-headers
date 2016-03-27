1d3aa27a8dd0adaa04b495450c0317f8c13e2c98
Romeo Van Snick <romeovs@gmail.com>
2016-03-27 10:42:48 +0200

    add last-modified validation

--- src/headers/last-modified/index.js
+++ src/headers/last-modified/index.js
@@ -8,4 +8,13 @@ export default {
 , merge (a, b) {
     return max(a, b) // return newest date
   }
+, validate (val) {
+    const warnings = []
+
+    if ( val > Date.now() ) {
+      warnings.push('setting a Last-Modified date that lies in the future')
+    }
+
+    return warnings
+  }
 }
