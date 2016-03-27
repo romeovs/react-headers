3d58c73b6ab19f6ff28cb8a45a234c79ca48260e
Romeo Van Snick <romeovs@gmail.com>
2016-03-27 10:07:49 +0200

    stringify headers

--- src/reduce.js
+++ src/reduce.js
@@ -1,6 +1,9 @@
-import merge from './merge'
-import parse from './parse'
+import merge     from './merge'
+import parse     from './parse'
+import stringify from './stringify'
 
 export default function (propsList) {
-  return propsList.reduce((acc, props) => merge(acc, parse(props)), {})
+  return propsList.reduce(function (acc, props) {
+    return stringify(merge(acc, parse(props)))
+  }, {})
 }
---
+++ src/stringify.js
@@ -0,0 +1,21 @@
+import headers from './headers'
+
+const def = val => val.toString()
+
+// stringify each field
+export default function (fields) {
+  return Object.keys(fields).reduce(function (acc, k) {
+    const header = headers[k]
+    if ( !header ) {
+      // we already warned in parse phase
+      return acc
+    } else {
+      const stringify = header.stringify || def
+      // merge headerss
+      return {
+        ...acc
+      , [k]: stringify(fields[k])
+      }
+    }
+  }, {})
+}
