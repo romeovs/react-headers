f01c8c9e8cd72644865dc9f143806598c08644fd
Romeo Van Snick <romeovs@gmail.com>
2016-03-27 09:54:41 +0200

    add lastModified to headers index

--- src/headers/index.js
+++ src/headers/index.js
@@ -1,7 +1,9 @@
 import cacheControl from './cache-control'
 import status       from './status'
+import lastModified from './last-modified'
 
 export default {
   cacheControl
+, lastModified
 , status
 }
