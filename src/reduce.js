a865f120a177adb21965d81859ec97d48d23a892
Romeo Van Snick <romeovs@gmail.com>
2016-03-27 10:42:28 +0200

    add validate step

--- src/reduce.js
+++ src/reduce.js
@@ -1,9 +1,10 @@
 import merge     from './merge'
 import parse     from './parse'
 import stringify from './stringify'
+import validate  from './validate'
 
 export default function (propsList) {
   return propsList.reduce(function (acc, props) {
-    return stringify(merge(acc, parse(props)))
+    return stringify(merge(acc, validate(parse(props))))
   }, {})
 }
