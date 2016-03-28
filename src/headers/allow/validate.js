23d5ca1b47f7addf6392e5576578f963ca3361a6
Romeo Van Snick <romeovs@gmail.com>
2016-03-28 11:03:51 +0200

    remove superfluous logs

--- src/headers/allow/validate.js
+++ src/headers/allow/validate.js
@@ -13,14 +13,11 @@ const known = [
 export default function (val) {
   const warnings = []
 
-  console.log('val', val)
   val.forEach(function (el) {
-    console.log(el)
     if ( known.indexOf(el) < 0 ) {
       warnings.push(`unknown request method: \`${el}\``)
     }
   })
 
-  console.log(warnings)
   return warnings
 }
