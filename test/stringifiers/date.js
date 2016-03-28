e2a4f4f85664fdafbfd66785a45aa0d496e08bd2
Romeo Van Snick <romeovs@gmail.com>
2016-03-28 17:24:37 +0200

    add date stringifier

---
+++ test/stringifiers/date.js
@@ -0,0 +1,8 @@
+import { expect } from 'chai'
+import undate     from '../src/stringifiers/date'
+
+describe('date stringification', () => {
+  it('should return the date is specified in RFC-1123', () => {
+    expect(undate(new Date(0))).to.equal('Thu, 01 Jan 1970 00:00:00 GMT')
+  })
+})
