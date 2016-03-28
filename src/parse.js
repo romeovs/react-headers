344c7fa07854a9b94b6ec1e95a9509297086596f
Romeo Van Snick <romeovs@gmail.com>
2016-03-28 11:20:54 +0200

    convert header fields to camelCase

--- src/parse.js
+++ src/parse.js
@@ -1,9 +1,15 @@
 // parse a set of props to a set of parsed headers
-import headers from './headers'
+import headers   from './headers'
+import {
+  fromSnake
+} from './helpers/case'
 
 const def = val => val
 export default function (props) {
-  return Object.keys(props).reduce((acc, k) => {
+  return Object.keys(props).reduce((acc, key) => {
+
+    const k = fromSnake(key)
+
     const header = headers[k]   || {}
     const parse  = header.parse || def
     // extend the result with the parsed field
