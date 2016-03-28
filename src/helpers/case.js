681b42731bbfc6b5e6bc1b64f713bcc35b011012
Romeo Van Snick <romeovs@gmail.com>
2016-03-28 11:14:54 +0200

    elaborate case functions with special cases and improve tests

--- src/helpers/case.js
+++ src/helpers/case.js
@@ -1,14 +1,31 @@
 
 
+const specialTo = {
+  contentmd5: 'content-MD5'
+, smaxage:    's-maxage'
+}
 
-const toRe = /([a-z])([A-Z])/g
-export const toSnake = function (str) {
-  return str.replace(toRe, $ => `${$[0]}-${$[1].toLowerCase()}`)
+const specialFrom = {
+  'content-md5': 'contentMD5'
 }
 
 
+// fooBar -> foo-bar
+const toRe = /([a-z])([A-Z])/g
+export const toSnake = function (str) {
+  const s = specialTo[str.toLowerCase()]
+  return s ? s : str.replace(toRe, $ => `${$[0]}-${$[1].toLowerCase()}`)
+}
+
+// foo-bar  -> fooBar
 const fromRe = /-([a-zA-Z])/g
 export const fromSnake = function (str) {
+  const s = specialFrom[str.toLowerCase()]
+  return s ? s : str.replace(fromRe, $ => $[1].toUpperCase())
+}
 
-  return str.replace(fromRe, $ => $[1].toUpperCase())
+const hdrRe = /(^[a-z]|-[a-z])/g
+export const toHeader = function (str) {
+  const snake = toSnake(str)
+  return snake.replace(hdrRe, $ => $.toUpperCase())
 }
--- test/case.js
+++ test/case.js
@@ -2,32 +2,51 @@ import { expect } from 'chai'
 import {
   toSnake
 , fromSnake
+, toHeader
 }  from '../src/helpers/case'
 
 
-
 const samples = {
-  'max-age': 'maxAge'
-, 's-maxage': 'sMaxage'
-, 'x-foo-bar': 'xFooBar'
-, 'content-md5': 'contentMd5'
+  allow: 'allow'
+, cacheControl: 'cache-control'
+, lastModified: 'last-modified'
+, maxAge: 'max-age'
+, sMaxage: 's-maxage'
+, xFooBar: 'x-foo-bar'
+, contentMD5: 'content-MD5'
 }
+
+const headers = {
+  allow: 'Allow'
+, cacheControl: 'Cache-Control'
+, lastModified: 'Last-Modified'
+, contentMD5: 'Content-MD5'
+}
+
 
 describe('case', () => {
 
   it('should convert to snake-case', () => {
     Object.keys(samples).forEach(key => {
-      const snake = key
-      const camel = samples[key]
+      const snake = samples[key]
+      const camel = key
       expect(toSnake(camel)).to.equal(snake)
     })
   })
 
   it('should convert from snake-case', () => {
     Object.keys(samples).forEach(key => {
-      const snake = key
-      const camel = samples[key]
+      const snake = samples[key]
+      const camel = key
       expect(fromSnake(snake)).to.equal(camel)
+    })
+  })
+
+  it('should convert to header case', () => {
+    Object.keys(headers).forEach(key => {
+      const camel  = key
+      const header = headers[key]
+      expect(toHeader(camel)).to.equal(header)
     })
   })
 
